821a8b85c061aa5b5998c262aaf7a474b9a3fb09
Semenov Sergei <dp120291ssv@gmail.com>
2022-08-30 21:01:09 +0300

    add basic pages + routing

---
+++ pages/contacts/contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <>
+            <h1>
+                Single Contact
+            </h1>
+            <p>
+                test@gmail.com
+            </p>
+        </>
+
+    );
+};
+
+export default Contact;
---
+++ pages/contacts/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <>
+            <h1>
+                 Contacts List
+            </h1>
+            <p>test@gmail.com</p>
+            <p>test@gmail.com</p>
+            <p>test@gmail.com</p>
+            <p>test@gmail.com</p>
+        </>
+
+    );
+};
+
+export default Contacts;
