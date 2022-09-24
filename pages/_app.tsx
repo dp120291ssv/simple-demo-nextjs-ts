138541b9f1bf04f46c5842a51be2832f1bd4e421
Semenov Sergei <dp120291ssv@gmail.com>
2022-09-24 19:04:45 +0300

    fetch data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import type {AppProps} from 'next/app'
 import Layout from "../components/Layout";
 import '../styles/globals.scss'
 import Image from 'next/image'
 import boratImg from '../public/borat.webp'
 import Head from "next/head";
-import React from "react";
 
 const MyApp = ({Component, pageProps}: AppProps) => (
     <Layout>
+        <Head>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner&family=Roboto:wght@300&display=swap"
+                rel="stylesheet"/>
+        </Head>
         <main>
             <Component {...pageProps} />
         </main>
--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,18 +1,63 @@
 import React from "react";
 import Heading from "../../components/Heading";
 import Head from "next/head";
+import {useEffect, useState} from "react";
 
-const Contacts = () => {
+export const getStaticProps = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    // const data = null;
+
+    /**
+     * для перехвата ошибок, например если data = null
+     * */
+    if(!data){
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {contacts: data}
+    }
+}
+
+export type Contact = {
+    id: string;
+    name: string;
+    email: string;
+}
+
+export type ContactsProps = {
+    contacts: Array<Contact>
+}
+
+const Contacts = ({contacts}: ContactsProps) => {
+    /**
+     * альтрернатива для getStaticProps, только getStaticProps делает ssr
+     * */
+    // const [contacts, setContacts] = useState(null)
+    //
+    // useEffect(() => {
+    //     const fetchData = async() => {
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    //         const data = await response.json();
+    //         setContacts(data);
+    //     }
+    //     fetchData();
+    // }, [])
+
     return (
         <>
             <Head>
                 <title>Contacts</title>
             </Head>
             <Heading text="Contacts List:" />
-            <p>test@gmail.com</p>
-            <p>test@gmail.com</p>
-            <p>test@gmail.com</p>
-            <p>test@gmail.com</p>
+            {contacts && contacts.map(({id, name, email}) => (
+                <li key={id}>
+                    <strong>{name}</strong> ({email})
+                </li>
+            ))}
         </>
     );
 };
