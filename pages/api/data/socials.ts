f15e48ec8c9baeed2468513023abf6fe45569a6f
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-01 18:53:44 +0300

    preparing API

---
+++ pages/api/data/socials.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    id: 1,
+    icon: 'youtube',
+    path: 'https://youtube.com',
+  },
+  {
+    id: 2,
+    icon: 'instagram',
+    path: 'https://instagram.com',
+  },
+  {
+    id: 3,
+    icon: 'linkedin',
+    path: 'https://www.linkedin.com/in/smnvsrg',
+  },
+  {
+    id: 4,
+    icon: 'twitter',
+    path: 'https://twitter.com',
+  },
+];
