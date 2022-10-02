94d59c4b2896570adfa5ce9280d18de662db87c1
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-02 10:20:39 +0300

    render socials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 import Heading from "../components/Heading";
 import styles from '../styles/Home.module.scss'
+import Head from "next/head";
 
-const Home = () => {
+export const getStaticProps = async () => {
+    const response = await fetch('http://localhost:3000/api/socials');
+    const data = await response.json();
+    // const data = null;
+    /**
+     * для перехвата ошибок, например если data = null
+     * */
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+    return {
+        props: {socials: data}
+    }
+}
+
+const Home = ({socials}) => {
     return (
         <div className={styles.wrapper}>
+            <Head>
+                <title>Home</title>
+            </Head>
             <Heading text="Demo Next.js application"/>
+            <Socials socials={socials}/>
         </div>
+
     );
 };
 
