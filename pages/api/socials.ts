b14872e259b085c0dd5729131f3cee5f4877ef86
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-04 19:47:42 +0300

    fix2

--- pages/api/socials.ts
+++ pages/api/socials.ts
@@ -1,14 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {socials} from './data/socials';
-import {SocialsType} from "../../types";
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<SocialsType>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
+        // console.log('res', res.json(socials))
         res.status(200).json(socials)
-        console.log('res', res.json(socials))
     }
 }
